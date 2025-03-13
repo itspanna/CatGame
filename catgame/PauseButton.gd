dfc03ae10abfd84583a90cb00a82c64e4bb66a45
Panna <145791814+itspanna@users.noreply.github.com>
2025-03-13 11:26:47 +0100

    Add files via upload

--- catgame/PauseButton.gd
+++ catgame/PauseButton.gd
@@ -4,9 +4,8 @@ extends Button
 
 func _ready():
 	connect("pressed", Callable(self, "_on_pause_pressed"))
-
-
-func _on_pressed() -> void:
+	
+func _on_pause_button_pressed():
 	print("Pause")
 	pause_menu.toggle_pause()
 	
